eb1e7ee037cbd36beb2a8198637573da94b80d94
gustavelson <78645956+gustavelson@users.noreply.github.com>
2021-08-11 12:28:02 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:28.99, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:28.99, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:37.99, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:34.99, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Marguerita', img:'images/pizza5.png', price:32.99, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:37.99, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:32.99, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
